23916e4f56c8a42c56c16f2e59cfe44c471a7144
Wesley Alves <wesley_alv@outlook.com>
2021-06-21 22:01:11 -0300

    refactor: config db connectiion

---
+++ ormconfig.js
@@ -0,0 +1,19 @@
+const { SimpleConsoleLogger } = require('typeorm')
+
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_BASE,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  logging: false,
+  logger: new SimpleConsoleLogger(false),
+  synchronize: false,
+  entities: ['src/entities/*.js'],
+  migrations: ['src/database/migrations/*.ts'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/database/migrations'
+  }
+}
