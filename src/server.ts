83d8a2021d1372f6b45591bd6e3e975407d0a156
Wesley Alves <wesley_alv@outlook.com>
2021-06-22 21:29:25 -0300

    refactor: implementing error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import 'reflect-metadata'
+import 'express-async-errors'
 
 import './database'
 import { router } from './routes'
@@ -10,5 +11,18 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 
 app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(500).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
 
 app.listen(port, () => console.log('Server is running'))
