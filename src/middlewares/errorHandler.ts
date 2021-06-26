8c09f6ceebc5af6733aae20c93006354b03717cf
Wesley Alves <wesley_alv@outlook.com>
2021-06-26 16:41:47 -0300

    refactor: config logger

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 import { ServiceError } from '../utils/ServiceError'
+import logger from '../logger'
 
 export function errorHandler(
   err: Error,
@@ -9,6 +10,8 @@ export function errorHandler(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ): Response {
+  logger.debug(err)
+
   if (err instanceof ServiceError) {
     return res.status(err.statusCode).json({ error: err.message })
   }
