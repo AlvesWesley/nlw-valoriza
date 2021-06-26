09003babffedf9baeed1400bde06b975d0184f23
Wesley Alves <wesley_alv@outlook.com>
2021-06-26 15:39:53 -0300

    test: add create compliement integration test

--- tests/integration/CreateCompliment.test.ts
+++ tests/integration/CreateCompliment.test.ts
@@ -4,12 +4,13 @@ import chaiHTTP from 'chai-http'
 import { getApplication } from '../helpers/utils/app'
 import { connectInDatabase, clearDatabase } from '../helpers/seeders/database'
 import { seedUsers } from '../helpers/seeders/user'
+import { seedTags } from '../helpers/seeders/tag'
 import * as stubJwt from '../helpers/stubs/jwt'
 
 chai.use(chaiHTTP)
 
-describe('List Users', function () {
-  const url = '/users'
+describe('Create User', function () {
+  const url = '/compliments'
 
   before(async () => {
     await connectInDatabase()
@@ -19,40 +20,43 @@ describe('List Users', function () {
     await clearDatabase()
   })
 
-  describe('resposta bem sucedida (200)', function () {
-    it('espera retornar um response com um array de user', async () => {
+  describe('resposta bem sucedida (201)', function () {
+    it('espera registrar um user retornar um response com os dados do user', async () => {
       const app = await getApplication()
-      const users = await seedUsers()
-      stubJwt.stubJwtVerify({ sub: users[0].id })
+      const [userSender, userReceiver] = await seedUsers()
+      const [tag] = await seedTags()
+      const compliement = {
+        message: 'Obrigado',
+        userSenderId: userSender.id,
+        userReceiverId: userReceiver.id,
+        tagId: tag.id
+      }
+      stubJwt.stubJwtVerify({ sub: userSender.id })
       const response = await chai
         .request(app)
-        .get(url)
+        .post(url)
+        .send(compliement)
         .set('Authorization', 'Bearer FAKE_TOKEN')
       stubJwt.restoreJwt()
 
-      expect(response.status).to.be.equal(200)
-      expect(response.body).to.be.eql(
-        users.map(user => {
-          const { id, name, email, admin, createdAt, updatedAt } = user
-          return {
-            id,
-            name,
-            email,
-            admin,
-            createdAt: createdAt.toISOString(),
-            updatedAt: updatedAt.toISOString()
-          }
-        })
-      )
+      expect(response.status).to.be.equal(201)
+      expect(response.body).to.deep.include(compliement)
     })
   })
 
   describe('resposta bem sucedida (401)', function () {
     it('espera retornar um response de erro por nao estar autenticado', async () => {
       const app = await getApplication()
+      const compliement = {
+        message: 'Obrigado',
+        userSenderId: '45b3d6ab-7327-4332-9e6c-8497367a176a',
+        userReceiverId: '6e29947c-31e2-4530-ab9b-e1a2e52bd0a9',
+        tagId: 'a0677868-a9a7-4137-bfcd-08e1ce408d56'
+      }
       const response = await chai
         .request(app)
-        .get(url)
+        .post(url)
+        .send(compliement)
         .set('Authorization', 'Bearer FAKE_TOKEN')
 
       expect(response.status).to.be.equal(401)
